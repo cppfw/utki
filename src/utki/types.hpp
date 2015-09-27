8928b99df1c308c350b437aa8b82be96476b2d78
Ivan Gagis <igagis@gmail.com>
2015-09-27 06:13:51 +0300

    stuff

--- src/utki/types.hpp
+++ src/utki/types.hpp
@@ -14,17 +14,25 @@ namespace utki{
 
 
 
-template <size_t type_size> struct uint_size{typedef std::uint64_t type;};
+template <size_t type_size> struct uint_size;
 template <> struct uint_size<1>{typedef std::uint8_t type;};
 template <> struct uint_size<2>{typedef std::uint16_t type;};
 template <> struct uint_size<3>{typedef std::uint32_t type;};
 template <> struct uint_size<4>{typedef std::uint32_t type;};
+template <> struct uint_size<5>{typedef std::uint64_t type;};
+template <> struct uint_size<6>{typedef std::uint64_t type;};
+template <> struct uint_size<7>{typedef std::uint64_t type;};
+template <> struct uint_size<8>{typedef std::uint64_t type;};
 
-template <size_t type_size> struct int_size{typedef std::int64_t type;};
+template <size_t type_size> struct int_size;
 template <> struct int_size<1>{typedef std::int8_t type;};
 template <> struct int_size<2>{typedef std::int16_t type;};
 template <> struct int_size<3>{typedef std::int32_t type;};
 template <> struct int_size<4>{typedef std::int32_t type;};
+template <> struct int_size<5>{typedef std::int64_t type;};
+template <> struct int_size<6>{typedef std::int64_t type;};
+template <> struct int_size<7>{typedef std::int64_t type;};
+template <> struct int_size<8>{typedef std::int64_t type;};
 
 
 
