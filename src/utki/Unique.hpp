f4f9873a717874e6e49514a68375c96d986f5dbe
Ivan Gagis <ivan.gagis@tieto.com>
2015-09-28 16:45:41 +0300

    stuff

---
+++ src/utki/Unique.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+namespace utki{
+
+
+template< class T, class... Args > std::unique_ptr<T> makeUnique(Args&&... args);
+
+/**
+ * @brief Unique.
+ * Forces usage of makeUnique() function to create an object.
+ */
+class Unique{
+	template< class T, class... Args > friend std::unique_ptr<T> utki::makeUnique(Args&&...);
+private:
+	static void* operator new(size_t size){
+		return ::operator new(size);
+	}
+	
+public:
+	virtual ~Unique()noexcept{}
+};
+
+/**
+ * @brief Construct new unique_ptr managed object.
+ * @param args - arguments of object class constructor.
+ * @return std::unique_ptr pointing to a newly created object.
+ */
+template< class T, class... Args > std::unique_ptr<T> makeUnique(Args&&... args){
+	static_assert(std::is_base_of<Unique, T>::value, "Class does not inherit utki::Unique, cannot create object.");
+	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+
+}
