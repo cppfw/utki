c1d26887cb1bb4d19bb4fdbb13a33a5aa7592b83
Ivan Gagis <igagis@gmail.com>
2015-11-04 20:54:58 +0200

    check for errors

--- nuget/build_nuget.ps1
+++ nuget/build_nuget.ps1
@@ -20,8 +20,13 @@ Get-ChildItem "." -Filter *.in | Foreach-Object{
 
 "%VS140COMNTOOLS%\VsMSBuildCmd.bat"
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=Debug /p:Platform=x86 /v:minimal /nologo
+If(!$?){exit 1}
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=Release /p:Platform=x86 /v:minimal /nologo
+If(!$?){exit 1}
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=Debug /p:Platform=x64 /v:minimal /nologo
+If(!$?){exit 1}
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=Release /p:Platform=x64 /v:minimal /nologo
+If(!$?){exit 1}
 Write-NuGetPackage nuget.autopkg
+If(!$?){exit 1}
 Pop-Location
