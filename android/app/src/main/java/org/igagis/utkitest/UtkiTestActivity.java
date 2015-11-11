157ba22d2e3eebd04bba7b199d24fa442d2fb466
Ivan Gagis <igagis@gmail.com>
2015-11-11 21:56:07 +0200

    draft android project files

---
+++ android/app/src/main/java/org/igagis/utkitest/UtkiTestActivity.java
@@ -0,0 +1,12 @@
+package org.igagis.utkitest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UtkiTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
