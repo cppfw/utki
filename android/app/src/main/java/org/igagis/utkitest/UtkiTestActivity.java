3eac8daf0cb5978909e88068ecbce7aad44b8401
Ivan Gagis <igagis@gmail.com>
2015-11-13 14:59:15 +0200

    gradle

--- android/app/src/main/java/org/igagis/utkitest/UtkiTestActivity.java
+++ android/app/src/main/java/org/igagis/utkitest/UtkiTestActivity.java
@@ -1,9 +1,9 @@
 package org.igagis.utkitest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class UtkiTestActivity extends AppCompatActivity {
+public class UtkiTestActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
