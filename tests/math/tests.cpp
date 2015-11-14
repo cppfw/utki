b9d3bf849f5fb8498bd5a0c5f4a696e22550b638
Ivan Gagis <igagis@gmail.com>
2015-11-14 02:24:32 +0200

    clang warnings fixed

--- tests/math/tests.cpp
+++ tests/math/tests.cpp
@@ -10,21 +10,21 @@ namespace TestBasicMathStuff{
 
 
 void Run(){
-	ASSERT_ALWAYS(sin((long double)(0)) == 0)
-	ASSERT_ALWAYS(abs(sin(utki::pi<long double>() / 2) - 1) < 0.00001)
-	ASSERT_ALWAYS(abs(sin(utki::pi<long double>())) < 0.00001)
-	ASSERT_ALWAYS(abs(sin(utki::pi<long double>() * 3 / 2) + 1) < 0.00001)
+	ASSERT_ALWAYS(std::sin((long double)(0)) == 0)
+	ASSERT_ALWAYS(std::abs(std::sin(utki::pi<long double>() / 2) - 1) < 0.00001)
+	ASSERT_ALWAYS(std::abs(std::sin(utki::pi<long double>())) < 0.00001)
+	ASSERT_ALWAYS(std::abs(std::sin(utki::pi<long double>() * 3 / 2) + 1) < 0.00001)
 
-	ASSERT_ALWAYS(cos((long double)(0)) == 1)
-	ASSERT_ALWAYS(abs(cos(utki::pi<long double>() / 2)) < 0.00001)
-	ASSERT_ALWAYS(abs(cos(utki::pi<long double>()) + 1) < 0.00001)
-	ASSERT_ALWAYS(abs(cos(utki::pi<long double>() * 3 / 2)) < 0.00001)
+	ASSERT_ALWAYS(std::cos((long double)(0)) == 1)
+	ASSERT_ALWAYS(std::abs(std::cos(utki::pi<long double>() / 2)) < 0.00001)
+	ASSERT_ALWAYS(std::abs(std::cos(utki::pi<long double>()) + 1) < 0.00001)
+	ASSERT_ALWAYS(std::abs(std::cos(utki::pi<long double>() * 3 / 2)) < 0.00001)
 
-	ASSERT_ALWAYS(exp((long double)(0)) == 1)
-	ASSERT_ALWAYS(abs(exp(utki::logOf2<long double>()) - 2) < 0.00001)
+	ASSERT_ALWAYS(std::exp((long double)(0)) == 1)
+	ASSERT_ALWAYS(std::abs(std::exp(utki::logOf2<long double>()) - 2) < 0.00001)
 
-	ASSERT_ALWAYS(log((long double)(1)) == 0)
-	ASSERT_ALWAYS(abs(log((long double)(2)) - utki::logOf2<long double>()) < 0.00001)
+	ASSERT_ALWAYS(std::log((long double)(1)) == 0)
+	ASSERT_ALWAYS(std::abs(std::log((long double)(2)) - utki::logOf2<long double>()) < 0.00001)
 }
 
 }//~namespace
