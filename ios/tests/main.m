66e5fc9674387764a09710e9cfef9a035cdeb876
Ivan Gagis <igagis@gmail.com>
2015-12-02 04:08:58 +0200

    stuff

--- ios/tests/main.m
+++ ios/tests/main.m
@@ -6,11 +6,8 @@
 //  Copyright © 2015 Ivan Gagis. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+
 
 int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+    return 0;
 }
