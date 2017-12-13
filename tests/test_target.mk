9ce66ada5a4db1391de8ca2ec34ec71a6ab3e9ce
Ivan Gagis <igagis@gmail.com>
2017-12-13 16:35:21 +0200

    updating build scripts

--- tests/test_target.mk
+++ tests/test_target.mk
@@ -3,7 +3,7 @@ this_test := $(word $(words $(this_dirs)),$(this_dirs))
 
 define this_rule
 test:: $(prorab_this_name)
-	@prorab-running-test.sh $(this_test)
-	@(cd $(d); ./tests); if [ $$$$? -ne 0 ]; then prorab-error.sh "test failed"; else prorab-passed.sh; fi
+	@myci-running-test.sh $(this_test)
+	@(cd $(d); ./tests); if [ $$$$? -ne 0 ]; then myci-error.sh "test failed"; else myci-passed.sh; fi
 endef
 $(eval $(this_rule))
