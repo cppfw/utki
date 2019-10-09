30a1e70301717c7dacd6a2c8906362bc5c9addc3
Ivan Gagis <igagis@gmail.com>
2019-10-10 00:24:51 +0300

    msvs update to tools v142

--- nuget/build_nuget.ps1
+++ nuget/build_nuget.ps1
@@ -18,7 +18,7 @@ Get-ChildItem "." -Filter *.in | Foreach-Object{
 }
 
 
-"%VS141COMNTOOLS%\VsMSBuildCmd.bat"
+"%VS142COMNTOOLS%\VsMSBuildCmd.bat"
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=Debug /p:Platform=x86 /v:minimal /nologo
 If(!$?){exit 1}
 msbuild ../msvs_solution/msvs_solution.sln /t:Rebuild /p:Configuration=Release /p:Platform=x86 /v:minimal /nologo
