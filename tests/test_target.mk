acbff78085a5fd2d1354989ae6813359d00d56f3
Ivan Gagis <igagis@gmail.com>
2019-12-20 17:45:44 +0200

    msys tests running fix

--- tests/test_target.mk
+++ tests/test_target.mk
@@ -1,9 +1,39 @@
-this_dirs := $(subst /, ,$(shell (cd $(d); pwd;)))
-this_test := $(word $(words $(this_dirs)),$(this_dirs))
+ifeq ($(os),windows)
+    # to avoid /C converted to C:\ need to escape it as //C
+    this_test_cmd := (cd $(d) && cmd //C 'set PATH=../../src/build;%PATH% && $$(notdir $$^)')
+else ifeq ($(os),macosx)
+    this_test_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/build ./$$(notdir $$^))
+else ifeq ($(os),linux)
+    this_test_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src/build ./$$(notdir $$^))
+else
+    $(error "Unknown OS")
+endif
+
+this_dirs := $(subst /, ,$(d))
+this_test := $(word $(words $(this_dirs)), $(this_dirs))
 
 define this_rule
 test:: $(prorab_this_name)
-	@myci-running-test.sh $(this_test)
-	@(cd $(d); LD_LIBRARY_PATH=../../src/build ./tests); if [ $$$$? -ne 0 ]; then myci-error.sh "test failed"; else myci-passed.sh; fi
+$(.RECIPEPREFIX)@myci-running-test.sh $(this_test)
+$(.RECIPEPREFIX)$(Q)$(this_test_cmd) || myci-error.sh "test failed"
+$(.RECIPEPREFIX)@myci-passed.sh
+endef
+$(eval $(this_rule))
+
+
+
+ifeq ($(os),windows)
+    # to avoid /C converted to C:\ need to escape it as //C
+    this_gdb_cmd := (cd $(d) && cmd //C 'set PATH=../../src/build;%PATH% && gdb $$(notdir $$^)')
+else ifeq ($(os),macosx)
+    this_gdb_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/build gdb ./$$(notdir $$^))
+else ifeq ($(os),linux)
+    this_gdb_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src/build gdb ./$$(notdir $$^))
+endif
+
+
+define this_rule
+gdb:: $(prorab_this_name)
+$(.RECIPEPREFIX)$(Q)$(this_gdb_cmd)
 endef
 $(eval $(this_rule))
