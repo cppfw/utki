80568e5aef1ea97a3132f38bb80dfbcf0e54b03d
Ivan Gagis <igagis@gmail.com>
2019-12-20 18:15:41 +0300

    test_string project added to msvs solution

--- src/utki/string.hpp
+++ src/utki/string.hpp
@@ -2,10 +2,14 @@
 
 #include <string>
 
+#include "config.hpp"
+
 namespace utki{
 
-
-__attribute__((format(printf, 1, 2))) std::string make_string(const char* format, ...);
+#if M_COMPILER != M_COMPILER_MSVC
+__attribute__((format(printf, 1, 2)))
+#endif
+std::string make_string(const char* format, ...);
 
 
 }
