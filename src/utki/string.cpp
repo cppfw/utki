21eb2a1c0c8563c2ab6ff38547363fe5b777c701
Ivan Gagis <igagis@gmail.com>
2019-12-20 19:45:45 +0200

    optimization for make_string() function

--- src/utki/string.cpp
+++ src/utki/string.cpp
@@ -1,28 +1,49 @@
 #include "string.hpp"
 
 #include <cstdarg>
+#include <array>
+
+#include "debug.hpp"
 
 using namespace utki;
 
 
 std::string utki::make_string(const char* format, ...){
-	va_list args1;
-	va_start(args1, format);
-    int size = vsnprintf(nullptr, 0, format, args1);
-	va_end(args1);
+	std::array<char, 0x400> buf; // first guess is that the resulting string will take less than 1k
 
-    std::string ret;
-    ret.resize(size);
+	std::string ret;
 
-	va_list args2;
-	va_start(args2, format);
-    vsnprintf(
-			&*ret.begin(),
-			ret.size() + 1, // NOTE: since C++11 it is guaranteed that std::string buffer has 1 extra character for terminating zero.
-			format,
-			args2
-		);
-	va_end(args2);
-	
+	auto buf_ptr = &*buf.begin();
+	auto buf_size = buf.size();
+
+	for(unsigned i = 0;; ++i){
+		va_list args;
+		va_start(args, format);
+		int size = vsnprintf(
+				buf_ptr,
+				buf_size,
+				format,
+				args
+			);
+		va_end(args);
+
+		if(size < 0){
+			throw std::logic_error("snprintf() failed");
+		}
+
+		if(decltype(buf_size)(size) >= buf_size){
+			// resulting string takes more than we have first guessed, allocate enough memory and try again
+			ret.resize(size);
+
+			buf_ptr = &*ret.begin();
+			buf_size = ret.size() + 1; // NOTE: C++11 guarantees that std::stding's internal buffeer has 1 extra byte for null nerminator.
+		}else{
+			ret = std::string(buf_ptr, size);
+			break;
+		}
+
+		ASSERT(i < 1)
+	}
+    
     return ret;
 }
--- tests/string/main.cpp
+++ tests/string/main.cpp
@@ -2,11 +2,74 @@
 #include "../../src/utki/debug.hpp"
 
 int main(int argc, char** argv){
+	// test string without format specifiers
+	{
+		auto str = utki::make_string(" ");
+
+		ASSERT_INFO_ALWAYS(str.length() == 1, "str.length = " << str.length())
+		ASSERT_INFO_ALWAYS(str == " ", "str = " << str)
+	}
+
+	// test short string
 	{
 		auto str = utki::make_string("Hello %d world!", 13);
 
 		ASSERT_INFO_ALWAYS(str.length() == 15, "str.length = " << str.length())
 		ASSERT_INFO_ALWAYS(str == "Hello 13 world!", "str = " << str)
+	}
+
+	// test long string over 1k
+	{
+		auto str = utki::make_string(
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef" 
+
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef" 
+
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef" 
+
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+				"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef" 
+
+				"Hello %d world!", 13
+			);
+
+		ASSERT_INFO_ALWAYS(str.length() == 1024 + 15, "str.length = " << str.length())
+		ASSERT_INFO_ALWAYS(
+				str == 
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef" 
+
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef" 
+
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef" 
+
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef"
+						"1234567890abcdef" "1234567890abcdef" "1234567890abcdef" "1234567890abcdef" 
+
+						"Hello 13 world!",
+				"str = " << str
+			)
 	}
 
 }
