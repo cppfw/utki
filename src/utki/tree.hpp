b35098635911179b8ca88b6c7ce712c8722f9f1d
Ivan Gagis <igagis@gmail.com>
2020-01-30 20:11:31 +0200

    tree traverser first implementation

--- src/utki/tree.hpp
+++ src/utki/tree.hpp
@@ -5,7 +5,7 @@
 namespace utki{
 
 //TODO: doxygen
-template <class T, template <class, class> class C = std::vector, template <class> class A = std::allocator > class tree{
+template <class T, template <class, class> class C = std::vector, template <class> class A = std::allocator> class tree{
 public:
     typedef T value_type;
     typedef C<tree, A<tree>> container_type;
@@ -70,5 +70,159 @@ public:
         return this->value == value;
     }
 };
+
+template <class T> class traverser{
+	T& roots;
+public:
+	traverser(T& roots) :
+			roots(roots)
+	{}
+
+	typedef typename std::conditional<std::is_const<T>::value, const typename T::value_type, typename T::value_type>::type value_type;
+	typedef value_type* pointer;
+	typedef const value_type* const_pointer;
+	typedef value_type& reference;
+	typedef const value_type& const_reference;
+	typedef size_t size_type;
+	typedef size_t difference_type;
+
+	template <bool Is_const> class iterator_internal{
+		typedef typename std::conditional<Is_const, const T, T>::type list_type;
+		std::vector<list_type*> c;
+		typedef typename std::conditional<Is_const, typename T::const_iterator, typename T::iterator>::type iterator_type;
+		std::vector<iterator_type> i;
+
+		template<class> friend class traverser;
+
+		iterator_internal(T& roots, iterator_type i){
+			this->c.push_back(&roots);
+			this->i.push_back(i);
+		}
+
+	public:
+		typedef typename std::conditional<Is_const, const traverser::value_type, traverser::value_type>::type value_type;
+		typedef value_type* pointer;
+		typedef const value_type* const_pointer;
+		typedef value_type& reference;
+		typedef const value_type& const_reference;
+		typedef std::bidirectional_iterator_tag iterator_category;
+		typedef size_t difference_type;
+
+		iterator_internal& operator++()noexcept{
+			ASSERT(!this->i.empty())
+			ASSERT(this->i.size() == this->c.size())
+
+			{
+				ASSERT(this->c.back())
+				auto& iter = this->i.back();
+
+				ASSERT(iter != this->c.back()->end())
+				if(!iter->children.empty()){
+					this->c.push_back(&iter->children);
+					this->i.push_back(this->c.back()->begin());
+					return *this;
+				}
+				++iter;
+			}
+
+			ASSERT(this->i.size() >= 1)
+			for(; this->i.size() != 1;){
+				if(this->i.back() == this->c.back()->end()){
+					this->c.pop_back();
+					this->i.pop_back();
+					++this->i.back();
+				}else{
+					break;
+				}
+			}
+			
+			return *this;
+		}
+
+		iterator_internal& operator--()noexcept{
+			ASSERT(!this->i.empty())
+			ASSERT(!this->c.empty())
+
+			if(this->i.back() == this->c.back()->begin()){
+				this->i.pop_back();
+				this->c.pop_back();
+				return *this;
+			}
+
+			for(;;){
+				auto& list = *this->c.back();
+				auto& iter = this->i.back();
+				ASSERT(iter != list.begin())
+				--iter;
+
+				if(!iter->children.empty()){
+					this->c.push_back(&iter->children);
+					this->i.push_back(this->c.back()->end());
+				}else{
+					break;
+				}
+			}
+
+			return *this;
+		}
+
+		bool operator==(const iterator_internal& iter)const noexcept{
+			return this->i == iter.i;
+		}
+
+		bool operator!=(const iterator_internal& iter)const noexcept{
+			return !this->operator==(iter);
+		}
+
+		typename std::conditional<Is_const, const typename T::value_type, typename T::value_type>::type& operator*()noexcept{
+			return *this->i.back();
+		}
+
+		typename std::conditional<Is_const, const typename T::value_type, typename T::value_type>::type* operator->()noexcept{
+			return this->i.back().operator->();
+		}
+	};
+
+	typedef iterator_internal<std::is_const<T>::value> iterator;
+	typedef iterator_internal<true> const_iterator;
+	typedef std::reverse_iterator<iterator> reverse_iterator;
+	typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+	iterator begin(){
+		return iterator(this->roots, this->roots.begin());
+	}
+
+	const_iterator cbegin()const{
+		return const_iterator(this->roots, this->roots.cbegin());
+	}
+
+	iterator end(){
+		return iterator(this->roots, this->roots.end());
+	}
+
+	const_iterator cend()const{
+		return const_iterator(this->roots, this->roots.cend());
+	}
+
+	const_reverse_iterator crbegin()const noexcept{
+		return const_reverse_iterator(this->cend());
+	}
+
+	const_reverse_iterator crend()const noexcept{
+		return const_reverse_iterator(this->cbegin());
+	}
+	
+	reverse_iterator rbegin()noexcept{
+		return reverse_iterator(this->end());
+	}
+
+	reverse_iterator rend()noexcept{
+		return reverse_iterator(this->begin());
+	}
+};
+
+template<class T> traverser<T> make_traverser(T& roots){
+	return traverser<T>(roots);
+}
 
 }
--- tests/tree/tests.cpp
+++ tests/tree/tests.cpp
@@ -3,13 +3,11 @@
 
 #include "tests.hpp"
 
-using namespace utki;
-
 
 namespace{
 void basic(){
 	{
-		tree<int> t;
+		utki::tree<int> t;
 
 		t.children.push_back(35);
 
@@ -17,7 +15,12 @@ void basic(){
 	}
 
 	{
-		tree<int> t{
+		utki::tree<int> t(1,{34, 45});
+		ASSERT_INFO_ALWAYS(t.value == 1, "t.value = " << t.value)
+	}
+
+	{
+		const utki::tree<int> t{
 			{34, 45},
 
 			{
@@ -47,7 +50,7 @@ void basic(){
 }
 
 void test_move_assignment(){
-	tree<int> t{
+	utki::tree<int> t{
 		{34, 45},
 
 		{
@@ -76,7 +79,7 @@ void test_move_assignment(){
 }
 
 void test_assignment(){
-	tree<int> t{
+	utki::tree<int> t{
 		{34, 45},
 
 		{
@@ -105,7 +108,7 @@ void test_assignment(){
 }
 
 void test_comparison(){
-	tree<int> t{
+	utki::tree<int> t{
 		{34, 45},
 
 		{
@@ -135,7 +138,7 @@ void test_comparison(){
 void test_constructors_from_container(){
 	// lvalue, lvalue
 	{
-		typedef tree<int> tree_int;
+		typedef utki::tree<int> tree_int;
 		tree_int::container_type children = {
 			tree_int(10),
 			tree_int(20)
@@ -151,7 +154,7 @@ void test_constructors_from_container(){
 
 	// lvalue, rvalue
 	{
-		typedef tree<int> tree_int;
+		typedef utki::tree<int> tree_int;
 		tree_int::container_type children = {
 			tree_int(10),
 			tree_int(20)
@@ -167,7 +170,7 @@ void test_constructors_from_container(){
 
 	// rvalue, rvalue
 	{
-		typedef tree<std::string> tree_str;
+		typedef utki::tree<std::string> tree_str;
 		tree_str::container_type children = {
 			tree_str("10"),
 			tree_str("20")
@@ -183,6 +186,314 @@ void test_constructors_from_container(){
 		ASSERT_INFO_ALWAYS(t.children[1].value == "20", "t.children[1].value = " << t.children[1].value)
 	}
 }
+
+void test_traverser(){
+	// non-const container
+	{
+		typedef utki::tree<int> tree;
+		tree::container_type roots{
+			tree(1,{34, 45}),
+
+			tree(2,{
+				tree(3, {78, 89, 96}),
+				tree(4,{32, 64, 128}),
+				tree(42, {98, 99, 100})
+			})
+		};
+
+		std::vector<int> encountered;
+
+		auto traverser = utki::make_traverser(roots);
+
+		ASSERT_INFO_ALWAYS(roots.begin()->value == 1, "roots.begin()->value = " << roots.begin()->value)
+
+		ASSERT_INFO_ALWAYS(traverser.begin()->value == 1, "traverser.begin()->value = " << traverser.begin()->value)
+
+		for(auto i = traverser.begin(); i != traverser.end(); ++i){
+			encountered.push_back(i->value);
+		}
+
+		std::vector<int> expected = {
+			1, 34, 45, 2, 3, 78, 89, 96, 4, 32, 64, 128, 42, 98, 99, 100
+		};
+
+		if(encountered != expected){
+			TRACE_ALWAYS(<< "encountered = ")
+			for(auto& i : encountered){
+				TRACE_ALWAYS(<< i << ", ")
+			}
+			TRACE_ALWAYS(<< std::endl)
+			ASSERT_INFO_ALWAYS(false, "traversed tree is not as expected")
+		}
+	}
+
+	// const container
+	{
+		typedef utki::tree<int> tree;
+		const tree::container_type roots{
+			tree(1,{34, 45}),
+
+			tree(2,{
+				tree(3, {78, 89, 96}),
+				tree(4,{32, 64, 128}),
+				tree(42, {98, 99, 100})
+			})
+		};
+
+		std::vector<int> encountered;
+
+		for(auto& i : utki::make_traverser(roots)){
+			encountered.push_back(i.value);
+		}
+
+		std::vector<int> expected{
+			1, 34, 45, 2, 3, 78, 89, 96, 4, 32, 64, 128, 42, 98, 99, 100
+		};
+
+		if(encountered != expected){
+			TRACE_ALWAYS(<< "encountered = ")
+			for(auto& i : encountered){
+				TRACE_ALWAYS(<< i << ", ")
+			}
+			TRACE_ALWAYS(<< std::endl)
+			ASSERT_ALWAYS(false)
+		}
+	}
+
+	// non-const container, const_iterator
+	{
+		typedef utki::tree<int> tree;
+		tree::container_type roots{
+			tree(1,{34, 45}),
+
+			tree(2,{
+				tree(3, {78, 89, 96}),
+				tree(4,{32, 64, 128}),
+				tree(42, {98, 99, 100})
+			})
+		};
+
+		std::vector<int> encountered;
+
+		auto traverser = utki::make_traverser(roots);
+
+		ASSERT_INFO_ALWAYS(roots.begin()->value == 1, "roots.begin()->value = " << roots.begin()->value)
+
+		ASSERT_INFO_ALWAYS(traverser.begin()->value == 1, "traverser.begin()->value = " << traverser.begin()->value)
+
+		for(auto i = traverser.cbegin(); i != traverser.cend(); ++i){
+			encountered.push_back(i->value);
+		}
+
+		std::vector<int> expected = {
+			1, 34, 45, 2, 3, 78, 89, 96, 4, 32, 64, 128, 42, 98, 99, 100
+		};
+
+		if(encountered != expected){
+			TRACE_ALWAYS(<< "encountered = ")
+			for(auto& i : encountered){
+				TRACE_ALWAYS(<< i << ", ")
+			}
+			TRACE_ALWAYS(<< std::endl)
+			ASSERT_INFO_ALWAYS(false, "traversed tree is not as expected")
+		}
+	}
+
+	// const container, const_iterator
+	{
+		typedef utki::tree<int> tree;
+		const tree::container_type roots{
+			tree(1,{34, 45}),
+
+			tree(2,{
+				tree(3, {78, 89, 96}),
+				tree(4,{32, 64, 128}),
+				tree(42, {98, 99, 100})
+			})
+		};
+
+		std::vector<int> encountered;
+
+		auto traverser = utki::make_traverser(roots);
+
+		ASSERT_INFO_ALWAYS(roots.begin()->value == 1, "roots.begin()->value = " << roots.begin()->value)
+
+		ASSERT_INFO_ALWAYS(traverser.begin()->value == 1, "traverser.begin()->value = " << traverser.begin()->value)
+
+		for(auto i = traverser.cbegin(); i != traverser.cend(); ++i){
+			encountered.push_back(i->value);
+		}
+
+		std::vector<int> expected = {
+			1, 34, 45, 2, 3, 78, 89, 96, 4, 32, 64, 128, 42, 98, 99, 100
+		};
+
+		if(encountered != expected){
+			TRACE_ALWAYS(<< "encountered = ")
+			for(auto& i : encountered){
+				TRACE_ALWAYS(<< i << ", ")
+			}
+			TRACE_ALWAYS(<< std::endl)
+			ASSERT_INFO_ALWAYS(false, "traversed tree is not as expected")
+		}
+	}
+
+	// non-const container, reverse_iterator
+	{
+		typedef utki::tree<int> tree;
+		tree::container_type roots{
+			tree(1,{34, 45}),
+
+			tree(2,{
+				tree(3, {78, 89, 96}),
+				tree(4,{32, 64, 128}),
+				tree(42, {98, 99, 100})
+			})
+		};
+
+		std::vector<int> encountered;
+
+		auto traverser = utki::make_traverser(roots);
+
+		ASSERT_INFO_ALWAYS(roots.begin()->value == 1, "roots.begin()->value = " << roots.begin()->value)
+
+		ASSERT_INFO_ALWAYS(traverser.begin()->value == 1, "traverser.begin()->value = " << traverser.begin()->value)
+
+		for(auto i = traverser.rbegin(); i != traverser.rend(); ++i){
+			encountered.push_back(i->value);
+		}
+
+		std::vector<int> expected = {
+			100, 99, 98, 42, 128, 64, 32, 4, 96, 89, 78, 3, 2, 45, 34, 1
+		};
+
+		if(encountered != expected){
+			TRACE_ALWAYS(<< "encountered = ")
+			for(auto& i : encountered){
+				TRACE_ALWAYS(<< i << ", ")
+			}
+			TRACE_ALWAYS(<< std::endl)
+			ASSERT_INFO_ALWAYS(false, "traversed tree is not as expected")
+		}
+	}
+
+	// const container, reverse_iterator
+	{
+		typedef utki::tree<int> tree;
+		const tree::container_type roots{
+			tree(1,{34, 45}),
+
+			tree(2,{
+				tree(3, {78, 89, 96}),
+				tree(4,{32, 64, 128}),
+				tree(42, {98, 99, 100})
+			})
+		};
+
+		std::vector<int> encountered;
+
+		auto traverser = utki::make_traverser(roots);
+
+		ASSERT_INFO_ALWAYS(roots.begin()->value == 1, "roots.begin()->value = " << roots.begin()->value)
+
+		ASSERT_INFO_ALWAYS(traverser.begin()->value == 1, "traverser.begin()->value = " << traverser.begin()->value)
+
+		for(auto i = traverser.rbegin(); i != traverser.rend(); ++i){
+			encountered.push_back(i->value);
+		}
+
+		std::vector<int> expected = {
+			100, 99, 98, 42, 128, 64, 32, 4, 96, 89, 78, 3, 2, 45, 34, 1
+		};
+
+		if(encountered != expected){
+			TRACE_ALWAYS(<< "encountered = ")
+			for(auto& i : encountered){
+				TRACE_ALWAYS(<< i << ", ")
+			}
+			TRACE_ALWAYS(<< std::endl)
+			ASSERT_INFO_ALWAYS(false, "traversed tree is not as expected")
+		}
+	}
+
+	// non-const container, const_reverse_iterator
+	{
+		typedef utki::tree<int> tree;
+		tree::container_type roots{
+			tree(1,{34, 45}),
+
+			tree(2,{
+				tree(3, {78, 89, 96}),
+				tree(4,{32, 64, 128}),
+				tree(42, {98, 99, 100})
+			})
+		};
+
+		std::vector<int> encountered;
+
+		auto traverser = utki::make_traverser(roots);
+
+		ASSERT_INFO_ALWAYS(roots.begin()->value == 1, "roots.begin()->value = " << roots.begin()->value)
+
+		ASSERT_INFO_ALWAYS(traverser.begin()->value == 1, "traverser.begin()->value = " << traverser.begin()->value)
+
+		for(auto i = traverser.crbegin(); i != traverser.crend(); ++i){
+			encountered.push_back(i->value);
+		}
+
+		std::vector<int> expected = {
+			100, 99, 98, 42, 128, 64, 32, 4, 96, 89, 78, 3, 2, 45, 34, 1
+		};
+
+		if(encountered != expected){
+			TRACE_ALWAYS(<< "encountered = ")
+			for(auto& i : encountered){
+				TRACE_ALWAYS(<< i << ", ")
+			}
+			TRACE_ALWAYS(<< std::endl)
+			ASSERT_INFO_ALWAYS(false, "traversed tree is not as expected")
+		}
+	}
+
+	// const container, const_reverse_iterator
+	{
+		typedef utki::tree<int> tree;
+		const tree::container_type roots{
+			tree(1,{34, 45}),
+
+			tree(2,{
+				tree(3, {78, 89, 96}),
+				tree(4,{32, 64, 128}),
+				tree(42, {98, 99, 100})
+			})
+		};
+
+		std::vector<int> encountered;
+
+		auto traverser = utki::make_traverser(roots);
+
+		ASSERT_INFO_ALWAYS(roots.begin()->value == 1, "roots.begin()->value = " << roots.begin()->value)
+
+		ASSERT_INFO_ALWAYS(traverser.begin()->value == 1, "traverser.begin()->value = " << traverser.begin()->value)
+
+		for(auto i = traverser.crbegin(); i != traverser.crend(); ++i){
+			encountered.push_back(i->value);
+		}
+
+		std::vector<int> expected = {
+			100, 99, 98, 42, 128, 64, 32, 4, 96, 89, 78, 3, 2, 45, 34, 1
+		};
+
+		if(encountered != expected){
+			TRACE_ALWAYS(<< "encountered = ")
+			for(auto& i : encountered){
+				TRACE_ALWAYS(<< i << ", ")
+			}
+			TRACE_ALWAYS(<< std::endl)
+			ASSERT_INFO_ALWAYS(false, "traversed tree is not as expected")
+		}
+	}
+}
 }
 
 
@@ -192,4 +503,5 @@ void test_utki_tree(){
 	test_move_assignment();
 	test_comparison();
 	test_constructors_from_container();
+	test_traverser();
 }
