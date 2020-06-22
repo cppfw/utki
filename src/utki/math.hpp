bf798e646f34b28c7ef66ad313dbe655e8bf0af1
Ivan Gagis <igagis@gmail.com>
2020-06-22 14:21:16 +0300

    added deg_to_rad() and rad_to_deg() functions

--- src/utki/math.hpp
+++ src/utki/math.hpp
@@ -1,16 +1,8 @@
-/**
- * @file math.hpp
- * @author Ivan Gagis <igagis@gmail.com>
- * @brief Math utilities.
- */
-
-
 #pragma once
 
 #include <cmath>
 
 #include "util.hpp"
-
 
 namespace utki{
 
@@ -27,91 +19,87 @@ namespace utki{
  * @return -1 if the argument is negative.
  * @return 1 if the number is positive.
  */
-template <typename T> inline T sign(T n)noexcept{
+template <typename T> constexpr T sign(T n)noexcept{
 	return n < 0 ? T(-1) : T(1);
 }
-
-
 
 /**
  * @brief Get number Pi.
  * @return number Pi.
  */
-template <typename T> inline T pi()noexcept{
+template <typename T> constexpr T pi()noexcept{
 	return T(3.14159265358979323846264338327950288);
 }
-
-
 
 /**
  * @brief Get 2 * Pi.
  * @return 2 * Pi.
  */
-template <typename T> inline T two_pi()noexcept{
+template <typename T> constexpr T two_pi()noexcept{
 	return T(2) * pi<T>();
 }
 
-//TODO: deprecated, remove.
-template <typename T> inline T twoPi()noexcept{
-	return two_pi<T>();
+/**
+ * @brief Convert angle degrees to radians.
+ * @param deg - angle in degrees.
+ * @return Angle in radians.
+ */
+template <typename T> constexpr T deg_to_rad(T deg)noexcept{
+	return pi<T>() * deg / T(180);
 }
 
-
+/**
+ * @brief Convert radians to angle degrees.
+ * @param rad - angle in radians.
+ * @return Angle in degrees.
+ */
+template <typename T> constexpr T rad_to_deg(T rad)noexcept{
+	return T(180) * rad / pi<T>();
+}
 
 /**
  * @brief Get natural logarithm of 2, i.e. ln(2).
  * @return natural logarithm of 2.
  */
-template <typename T> inline T log_2()noexcept{
+template <typename T> constexpr T log_2()noexcept{
 	return T(0.693147181);
 }
-
-//TODO: deprecated, remove.
-template <typename T> inline T logOf2()noexcept{
-	return log_2<T>();
-}
-
-
-
-//Power functions
 
 /**
  * @brief Calculate x^2.
  * @param x - value.
  * @return x * x.
  */
-template <typename T> inline T pow2(T x)noexcept{
+template <typename T> constexpr T pow2(T x)noexcept{
 	return x * x;
 }
 
 /**
  * @brief Calculate x^3.
  */
-template <typename T> inline T pow3(T x)noexcept{
+template <typename T> constexpr T pow3(T x)noexcept{
 	return pow2(x) * x;
 }
 
 /**
  * @brief Calculate x^4.
  */
-template <typename T> inline T pow4(T x)noexcept{
+template <typename T> constexpr T pow4(T x)noexcept{
 	return pow2(pow2(x));
 }
 
 /**
  * @brief Calculate x^5.
  */
-template <typename T> inline T pow5(T x)noexcept{
+template <typename T> constexpr T pow5(T x)noexcept{
 	return pow4(x) * x;
 }
 
 /**
  * @brief Calculate x^6.
  */
-template <typename T> inline T pow6(T x)noexcept{
+template <typename T> constexpr T pow6(T x)noexcept{
 	return pow2(pow3(x));
 }
-
-
 
 }
--- tests/math/tests.cpp
+++ tests/math/tests.cpp
@@ -3,11 +3,7 @@
 
 #include "tests.hpp"
 
-
-
-
 namespace TestBasicMathStuff{
-
 
 void Run(){
 	ASSERT_ALWAYS(std::sin((long double)(0)) == 0)
@@ -21,10 +17,13 @@ void Run(){
 	ASSERT_ALWAYS(std::abs(std::cos(utki::pi<long double>() * 3 / 2)) < 0.00001)
 
 	ASSERT_ALWAYS(std::exp((long double)(0)) == 1)
-	ASSERT_ALWAYS(std::abs(std::exp(utki::logOf2<long double>()) - 2) < 0.00001)
+	ASSERT_ALWAYS(std::abs(std::exp(utki::log_2<long double>()) - 2) < 0.00001)
 
 	ASSERT_ALWAYS(std::log((long double)(1)) == 0)
-	ASSERT_ALWAYS(std::abs(std::log((long double)(2)) - utki::logOf2<long double>()) < 0.00001)
+	ASSERT_ALWAYS(std::abs(std::log((long double)(2)) - utki::log_2<long double>()) < 0.00001)
+
+	ASSERT_ALWAYS(utki::rad_to_deg(utki::pi<float>()) == 180.0)
+	ASSERT_ALWAYS(utki::deg_to_rad(float(180)) == utki::pi<float>())
 }
 
-}//~namespace
+}
