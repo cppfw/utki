5585a96afaed4a827684d8bdcea18ef6a71162bc
Ivan Gagis <igagis@gmail.com>
2021-01-19 19:07:20 +0200

    Config (#12)

---
+++ config/base/base.mk
@@ -0,0 +1,15 @@
+this_cxxflags += -Wall # enable all warnings
+this_cxxflags += -Wnon-virtual-dtor # warn if base class has non-virtual destructor
+this_cxxflags += -Werror # treat warnings as errors
+# this_cxxflags += -Wfatal-errors # stop on first error encountered
+this_cxxflags += -fstrict-aliasing # in order to comply with the c++ standard more strictly
+this_cxxflags += -std=c++17
+this_cxxflags += -fPIC
+this_cxxflags += -g
+
+this_ldlibs += -lstdc++
+
+ifeq ($(gprof), true)
+    this_cxxflags += -pg
+    this_ldflags += -pg
+endif
--- tests/test_target.mk
+++ tests/test_target.mk
@@ -1,10 +1,10 @@
 ifeq ($(os),windows)
     # to avoid /C converted to C:\ need to escape it as //C
-    this_test_cmd := (cd $(d) && cmd //C 'set PATH=../../src/build;%PATH% && $$(notdir $$^)')
+    this_test_cmd := (cd $$(dir $$^) && cmd //C 'set PATH=../../../../src/out/$(c);%PATH% && $$(notdir $$^)')
 else ifeq ($(os),macosx)
-    this_test_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/build ./$$(notdir $$^))
+    this_test_cmd := (cd $$(dir $$^) && DYLD_LIBRARY_PATH=../../../../src/out/$(c):. ./$$(notdir $$^))
 else ifeq ($(os),linux)
-    this_test_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src/build ./$$(notdir $$^))
+    this_test_cmd := (cd $$(dir $$^) && LD_LIBRARY_PATH=../../../../src/out/$(c):. ./$$(notdir $$^))
 else
     $(error "Unknown OS")
 endif
@@ -15,7 +15,7 @@ this_test := $(word $(words $(this_dirs)), $(this_dirs))
 define this_rule
 test:: $(prorab_this_name)
 $(.RECIPEPREFIX)@myci-running-test.sh $(this_test)
-$(.RECIPEPREFIX)$(Q)$(this_test_cmd) || myci-error.sh "test failed"
+$(.RECIPEPREFIX)$(a)$(this_test_cmd) || myci-error.sh "test failed"
 $(.RECIPEPREFIX)@myci-passed.sh
 endef
 $(eval $(this_rule))
@@ -24,16 +24,15 @@ $(eval $(this_rule))
 
 ifeq ($(os),windows)
     # to avoid /C converted to C:\ need to escape it as //C
-    this_gdb_cmd := (cd $(d) && cmd //C 'set PATH=../../src/build;%PATH% && gdb $$(notdir $$^)')
+    this_gdb_cmd := (cd $$(dir $$^) && cmd //C 'set PATH=../../../../src/out/$(c);%PATH% && gdb $$(notdir $$^)')
 else ifeq ($(os),macosx)
-    this_gdb_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/build gdb ./$$(notdir $$^))
+    this_gdb_cmd := (cd $$(dir $$^) && DYLD_LIBRARY_PATH=../../../../src/out/$(c) gdb ./$$(notdir $$^))
 else ifeq ($(os),linux)
-    this_gdb_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src/build gdb ./$$(notdir $$^))
+    this_gdb_cmd := (cd $$(dir $$^) && LD_LIBRARY_PATH=../../../../src/out/$(c) gdb ./$$(notdir $$^))
 endif
-
 
 define this_rule
 gdb:: $(prorab_this_name)
-$(.RECIPEPREFIX)$(Q)$(this_gdb_cmd)
+$(.RECIPEPREFIX)$(a)$(this_gdb_cmd)
 endef
 $(eval $(this_rule))
