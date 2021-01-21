1e006b0aff0dd818da8cfe9561acef3926ac14f3
Ivan Gagis <igagis@gmail.com>
2021-01-21 17:53:47 +0200

    linq select fuction

---
+++ src/utki/linq.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <functional>
+
+#include "debug.hpp"
+
+namespace utki{
+
+#if __cplusplus >= 201703L
+template <
+		template <typename> typename C,
+		typename T,
+		typename F
+	>
+auto select(C<T>&& collection, F func){
+	C<decltype(func(std::move(collection[0])))> ret;
+
+	for(auto&& e : collection){
+		ret.push_back(func(std::move(e)));
+	}
+
+	return ret;
+}
+
+template <
+		template <typename> typename C,
+		typename T,
+		typename F
+	>
+auto select(const C<T>& collection, F func){
+	C<decltype(func(collection[0]))> ret;
+
+	for(auto&& e : collection){
+		ret.push_back(func(std::move(e)));
+	}
+
+	return ret;
+}
+#endif
+
+}
---
+++ tests/linq/tests.cpp
@@ -0,0 +1,62 @@
+#include "../../src/utki/linq.hpp"
+#include "../../src/utki/debug.hpp"
+
+#include "tests.hpp"
+
+namespace{
+void test_select(){
+#if __cplusplus >= 201703L
+	// test select(&&)
+	{
+		std::vector<std::pair<int, std::string>> in = {{
+			{13, "13"},
+			{14, "14"},
+			{1, "1"}
+		}};
+
+		auto out = utki::select(
+				std::move(in),
+				[](auto v){
+					return std::make_pair(std::move(v.second), 13.4f);
+				}
+			);
+
+		decltype(out) expected = {{
+			{"13", 13.4f},
+			{"14", 13.4f},
+			{"1", 13.4f}
+		}};
+
+		ASSERT_ALWAYS(out == expected)
+	}
+
+	// test select(const&)
+	{
+		std::vector<std::pair<int, std::string>> in = {{
+			{13, "13"},
+			{14, "14"},
+			{1, "1"}
+		}};
+
+		auto out = utki::select(
+				in,
+				[](auto v){
+					return std::make_pair(v.second, 13.4f);
+				}
+			);
+
+		decltype(out) expected = {{
+			{"13", 13.4f},
+			{"14", 13.4f},
+			{"1", 13.4f}
+		}};
+
+		ASSERT_ALWAYS(out == expected)
+	}
+#endif
+}
+}
+
+void test_utki_linq(){
+	test_select();
+}
