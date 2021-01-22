9d1553cee410afb9a1626f9ef536467890521bd9
Ivan Gagis <igagis@gmail.com>
2021-01-22 15:51:18 +0200

    stuff

--- src/utki/linq.hpp
+++ src/utki/linq.hpp
@@ -9,11 +9,12 @@ namespace utki{
 #if __cplusplus >= 201703L
 template <
 		template <typename...> typename C,
-		typename T,
+		typename... T,
 		typename F
 	>
-auto select(C<T>&& collection, F func){
+auto select(C<T...>&& collection, F func){
 	C<decltype(func(std::move(collection[0])))> ret;
+	ret.reserve(collection.size());
 
 	for(auto&& e : collection){
 		ret.push_back(func(std::move(e)));
@@ -24,11 +25,12 @@ auto select(C<T>&& collection, F func){
 
 template <
 		template <typename...> typename C,
-		typename T,
+		typename... T,
 		typename F
 	>
-auto select(const C<T>& collection, F func){
+auto select(const C<T...>& collection, F func){
 	C<decltype(func(collection[0]))> ret;
+	ret.reserve(collection.size());
 
 	for(auto& e : collection){
 		ret.push_back(func(e));
@@ -36,6 +38,34 @@ auto select(const C<T>& collection, F func){
 
 	return ret;
 }
+
+template <
+		template <typename...> typename C,
+		typename... T
+	>
+class linq{
+	C<T...> vec;
+public:
+	linq(C<T...>&& vec) :
+			vec(std::move(vec))
+	{}
+
+	C<T...> get(){
+		return std::move(this->vec);
+	}
+
+	template <typename F> auto select(F func){
+		std::vector<decltype(func(std::move(this->vec[0])))> ret;
+		ret.reserve(this->vec.size());
+
+		for(auto&& e : this->vec){
+			ret.push_back(func(std::move(e)));
+		}
+
+		return linq<std::vector, typename decltype(ret)::value_type>(std::move(ret));
+	}
+};
+
 #endif
 
 }
--- tests/linq/tests.cpp
+++ tests/linq/tests.cpp
@@ -16,12 +16,12 @@ void test_select(){
 			{1, "1"}
 		}};
 
-		auto out = utki::select(
-				std::move(in),
+		auto out = utki::linq(std::move(in)).select(
 				[](auto v){
+					TRACE(<< "v.second = " << v.second << std::endl)
 					return std::make_pair(std::move(v.second), 13.4f);
 				}
-			);
+			).get();
 
 		decltype(out) expected = {{
 			{"13", 13.4f},
@@ -30,6 +30,7 @@ void test_select(){
 		}};
 
 		ASSERT_ALWAYS(out == expected)
+		ASSERT_ALWAYS(in.empty())
 	}
 
 	// test select(const&)
@@ -54,6 +55,7 @@ void test_select(){
 		}};
 
 		ASSERT_ALWAYS(out == expected)
+		ASSERT_ALWAYS(!in[0].second.empty())
 	}
 #endif
 }
