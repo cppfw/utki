23a373950ea03815ae80dbdbb2193e9ebd9acda4
Ivan Gagis <igagis@gmail.com>
2021-05-26 17:12:05 +0300

    try static_cast to make MSVC compiler happy

--- src/utki/flags.hpp
+++ src/utki/flags.hpp
@@ -261,7 +261,7 @@ template <class T> flags<T> make_flags(std::initializer_list<T> initially_set_fl
 
 template <class enum_type>
 typename std::enable_if<
-		std::is_enum<enum_type>::value && (int(enum_type::enum_size) >= 0),
+		std::is_enum<enum_type>::value && (static_cast<int>(enum_type::enum_size) >= 0),
 		utki::flags<enum_type>
 	>::type
 operator|(enum_type e1, enum_type e2){
