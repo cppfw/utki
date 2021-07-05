2d3b92c87f10d84328c6413e4010ccaabf1cc907
Ivan Gagis <igagis@gmail.com>
2021-07-06 00:38:08 +0300

    debug unit test

--- tests/debug/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "main.hpp"
-
-
-
-int main(int argc, char *argv[]){
-	TestTingDebug();
-	
-	return 0;
-}
--- tests/debug/tests.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace TestBasicDebugStuff{
-void Run();
-}//~namespace
