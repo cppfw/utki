3fc619d106b61164dfd98bb1f650b81c3f1d7a65
Ivan Gagis <igagis@gmail.com>
2021-12-07 21:23:20 +0200

    add singleton test

---
+++ tests/unit/src/singleton.cpp
@@ -0,0 +1,28 @@
+#include <tst/set.hpp>
+#include <tst/check.hpp>
+
+#include <utki/singleton.hpp>
+
+namespace{
+class test_singleton : public utki::singleton<test_singleton>{
+public:
+	int a = 13;
+};
+}
+
+namespace{
+tst::set set("singleton", [](tst::suite& suite){
+	suite.add(
+		"only_one_singleton_instance_can_exist_at_a_time",
+		tst::flag::no_parallel,
+		[]{
+			test_singleton sing1;
+
+			try{
+				test_singleton sing2;
+				tst::check(false, SL) << "creating second singleton object should throw";
+			}catch(std::logic_error&){}
+		}
+	);
+});
+}
