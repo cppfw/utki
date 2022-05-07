604281b1a43eb1f19e517233c604d86bcbf3476b
Ivan Gagis <igagis@gmail.com>
2022-05-07 22:18:19 +0300

    stuff

--- src/utki/shared.hpp
+++ src/utki/shared.hpp
@@ -31,20 +31,20 @@ SOFTWARE.
 #include "config.hpp"
 #include "debug.hpp"
 
-namespace utki{
+namespace utki {
 
 /**
  * @brief Base class for objects managed by std::shared_ptr.
  * This class is to be used as virtual base class for cases when shared_from_this()
  * is needed in case of multiple inheritance.
  */
-class shared : public std::enable_shared_from_this<shared>{
-	static void* operator new(size_t size){
+class shared : public std::enable_shared_from_this<shared> {
+	static void* operator new(size_t size) {
 		return ::operator new(size);
 	}
-	
+
 public:
-	virtual ~shared()noexcept{}
+	virtual ~shared() noexcept {}
 };
 
 /**
@@ -52,7 +52,8 @@ public:
  * @param ptr - shared_ptr out of which to make weak_ptr.
  * @return std::weak_ptr created from given std::shared_ptr.
  */
-template <class T> std::weak_ptr<T> make_weak(const std::shared_ptr<T>& ptr){
+template <class T>
+std::weak_ptr<T> make_weak(const std::shared_ptr<T>& ptr) {
 	return std::weak_ptr<T>(ptr);
 }
 
@@ -64,7 +65,8 @@ template <class T> std::weak_ptr<T> make_weak(const std::shared_ptr<T>& ptr){
  * to the template type. Thus, one does not have to do the dynamic cast manually.
  * @return shared pointer to the given shared object.
  */
-template <class T> std::shared_ptr<T> make_shared_from(T& o){
+template <class T>
+std::shared_ptr<T> make_shared_from(T& o) {
 	static_assert(std::is_base_of<shared, T>::value, "make_shared_from(): argument must be derived from utki::shared");
 	return std::dynamic_pointer_cast<T>(o.shared_from_this());
 }
@@ -77,9 +79,10 @@ template <class T> std::shared_ptr<T> make_shared_from(T& o){
  * to the template type and then converts to weak pointer.
  * @return weak pointer to the given shared object.
  */
-template <class T> std::weak_ptr<T> make_weak_from(T& o){
+template <class T>
+std::weak_ptr<T> make_weak_from(T& o) {
 	static_assert(std::is_base_of<shared, T>::value, "make_shared_from(): argument must be derived from utki::shared");
 	return utki::make_weak(utki::make_shared_from(o));
 }
 
-}
+} // namespace utki
