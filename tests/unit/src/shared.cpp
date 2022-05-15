cee11b90429cef164fea80f87ce8027abe889b0f
Ivan Gagis <igagis@gmail.com>
2022-05-15 22:49:24 +0300

    stuff

--- tests/unit/src/shared.cpp
+++ tests/unit/src/shared.cpp
@@ -1,63 +1,55 @@
-#include <tst/set.hpp>
 #include <tst/check.hpp>
-
+#include <tst/set.hpp>
 #include <utki/shared.hpp>
 
-namespace{
-class TestClass : public utki::shared{
+namespace {
+class TestClass : public utki::shared
+{
 public:
 	int a = 4;
-	
-	TestClass(){
-		
-	}
-	
-	TestClass(int i) : a(i) {}
-	
-	std::shared_ptr<TestClass> getPtr(){
+
+	TestClass() {}
+
+	TestClass(int i) :
+		a(i)
+	{}
+
+	std::shared_ptr<TestClass> getPtr()
+	{
 		return utki::make_shared_from(*this);
 	}
 };
-}
+} // namespace
 
-namespace{
-tst::set set("shared", [](tst::suite& suite){
-	suite.add(
-		"basic",
-		[](){
-			std::shared_ptr<TestClass> p1 = std::make_shared<TestClass>();
-			
-			std::shared_ptr<TestClass> p2 = std::make_shared<TestClass>(21);
-			
-			tst::check_eq(p1->a, 4, SL);
-			tst::check_eq(p2->a, 21, SL);
-			
-			tst::check_eq(p2->getPtr().operator->(), p2.operator->(), SL);
-		}
-	);
+namespace {
+tst::set set("shared", [](tst::suite& suite) {
+	suite.add("basic", []() {
+		std::shared_ptr<TestClass> p1 = std::make_shared<TestClass>();
 
-	suite.add(
-		"make_shared_from",
-		[](){
-			auto o = std::make_shared<TestClass>();
-			tst::check(o, SL);
+		std::shared_ptr<TestClass> p2 = std::make_shared<TestClass>(21);
 
-			auto sft = utki::make_shared_from(*o);
-			tst::check(sft, SL);
-			tst::check_eq(sft->a, 4, SL);
-		}
-	);
+		tst::check_eq(p1->a, 4, SL);
+		tst::check_eq(p2->a, 21, SL);
 
-	suite.add(
-		"make_weak_from",
-		[](){
-			auto o = std::make_shared<TestClass>();
-			tst::check(o, SL);
+		tst::check_eq(p2->getPtr().operator->(), p2.operator->(), SL);
+	});
 
-			auto sft = utki::make_weak_from(*o);
-			tst::check(sft.lock(), SL);
-			tst::check_eq(sft.lock()->a, 4, SL);
-		}
-	);
+	suite.add("make_shared_from", []() {
+		auto o = std::make_shared<TestClass>();
+		tst::check(o, SL);
+
+		auto sft = utki::make_shared_from(*o);
+		tst::check(sft, SL);
+		tst::check_eq(sft->a, 4, SL);
+	});
+
+	suite.add("make_weak_from", []() {
+		auto o = std::make_shared<TestClass>();
+		tst::check(o, SL);
+
+		auto sft = utki::make_weak_from(*o);
+		tst::check(sft.lock(), SL);
+		tst::check_eq(sft.lock()->a, 4, SL);
+	});
 });
-}
+} // namespace
--- tests/unit/src/singleton.cpp
+++ tests/unit/src/singleton.cpp
@@ -1,32 +1,33 @@
-#include <tst/set.hpp>
 #include <tst/check.hpp>
-
+#include <tst/set.hpp>
 #include <utki/config.hpp>
 #include <utki/singleton.hpp>
 
 #if M_COMPILER != M_COMPILER_MSVC
 
-namespace{
-class test_singleton : public utki::singleton<test_singleton>{
+namespace {
+class test_singleton : public utki::singleton<test_singleton>
+{
 public:
 	int a = 13;
 };
-}
+} // namespace
 
-namespace{
-tst::set set("singleton", [](tst::suite& suite){
-	suite.add(
+namespace {
+tst::set set("singleton", [](tst::suite& suite) {
+	suite.add( //
 		"only_one_singleton_instance_can_exist_at_a_time",
 		tst::flag::no_parallel,
-		[]{
+		[] {
 			test_singleton sing1;
 
-			try{
+			try {
 				test_singleton sing2;
 				tst::check(false, SL) << "creating second singleton object should throw";
-			}catch(std::logic_error&){}
+			} catch (std::logic_error&) {
+			}
 		}
 	);
 });
-}
+} // namespace
 #endif // ~msvc compiler
