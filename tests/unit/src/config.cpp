9925282c53807e9fd63535cd629e1c41bb9b039f
Ivan Gagis <ivan.gagis@ge.com>
2023-06-01 12:18:42 +0300

    add CFG_ENDIANNESS macros

---
+++ tests/unit/src/config.cpp
@@ -0,0 +1,23 @@
+#include <tst/check.hpp>
+#include <tst/set.hpp>
+#include <utki/config.hpp>
+
+namespace {
+tst::set set("config", [](tst::suite& suite) {
+	suite.add("endianness", []() {
+		unsigned integer = 1;
+		static_assert(sizeof(integer) >= 2, "1 byte integer detected");
+		auto p = reinterpret_cast<const uint8_t*>(&integer);
+
+		bool is_little_endian = (*p == 1);
+
+		if (is_little_endian) {
+			tst::check(CFG_ENDIANNESS == CFG_ENDIANNESS_LITTLE, SL);
+			tst::check(CFG_ENDIANNESS != CFG_ENDIANNESS_BIG, SL);
+		} else {
+			tst::check(CFG_ENDIANNESS == CFG_ENDIANNESS_BIG, SL);
+			tst::check(CFG_ENDIANNESS != CFG_ENDIANNESS_LITTLE, SL);
+		}
+	});
+});
+} // namespace
