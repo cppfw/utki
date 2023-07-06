d176ba4583f3b285374b6cadc38a3e715ecd9c52
Ivan Gagis <ivan.gagis@ge.com>
2023-07-06 11:09:22 +0300

    fix build

--- tests/unit/src/math.cpp
+++ tests/unit/src/math.cpp
@@ -8,15 +8,16 @@ const tst::set set("math", [](tst::suite& suite) {
 		constexpr auto epsilon = 0.00001;
 		constexpr auto two_pi_degrees = 180.0;
 
+		// clang-format off
 		tst::check_eq(std::sin((long double)(0)), (long double)(0), SL);
 		tst::check_lt(std::abs(std::sin((long double)(utki::pi) / 2) - 1), (long double)(epsilon), SL);
 		tst::check_lt(std::abs(std::sin((long double)(utki::pi))), (long double)(epsilon), SL);
-		tst::check_lt(std::abs(std::sin((long double)(utki::pi)*3 / 2) + 1), (long double)(epsilon), SL);
+		tst::check_lt(std::abs(std::sin((long double)(utki::pi) * 3 / 2) + 1), (long double)(epsilon), SL);
 
 		tst::check_eq(std::cos((long double)(0)), (long double)(1), SL);
 		tst::check_lt(std::abs(std::cos((long double)(utki::pi) / 2)), (long double)(epsilon), SL);
 		tst::check_lt(std::abs(std::cos((long double)(utki::pi)) + 1), (long double)(epsilon), SL);
-		tst::check_lt(std::abs(std::cos((long double)(utki::pi)*3 / 2)), (long double)(epsilon), SL);
+		tst::check_lt(std::abs(std::cos((long double)(utki::pi) * 3 / 2)), (long double)(epsilon), SL);
 
 		tst::check_eq(std::exp((long double)(0)), (long double)(1), SL);
 		tst::check_lt(std::abs(std::exp((long double)(utki::log_2)) - 2), (long double)(epsilon), SL);
@@ -26,6 +27,7 @@ const tst::set set("math", [](tst::suite& suite) {
 
 		tst::check_eq(utki::rad_to_deg(float(utki::pi)), float(two_pi_degrees), SL);
 		tst::check_eq(utki::deg_to_rad(float(two_pi_degrees)), float(utki::pi), SL);
+		// clang-format on
 	});
 });
 } // namespace
