c6ff519a0e858f6cf947972f83a42131d176ab63
Ivan Gagis <igagis@gmail.com>
2024-05-19 22:17:13 +0300

    add deserializator

---
+++ src/utki/deserializator.cpp
@@ -0,0 +1,135 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015-2024 Ivan Gagis <igagis@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/* ================ LICENSE END ================ */
+
+#include "deserializator.hpp"
+
+#include "string.hpp"
+#include "util.hpp"
+
+using namespace utki;
+
+std::string_view deserializator::read_string(size_t length)
+{
+	if (this->size() < length) {
+		throw std::invalid_argument(
+			utki::cat("deserializator::read_string(", length, "): buffer size is only ", this->size())
+		);
+	}
+
+	auto ret = utki::make_string_view(this->data.subspan(0, length));
+	this->data = this->data.subspan(length);
+	return ret;
+}
+
+utki::span<const uint8_t> deserializator::read_span(size_t length)
+{
+	if (this->size() < length) {
+		throw std::invalid_argument(
+			utki::cat("deserializator::read_span(", length, "): buffer size is only ", this->size())
+		);
+	}
+
+	auto ret = this->data.subspan(0, length);
+	this->data = this->data.subspan(length);
+	return ret;
+}
+
+uint16_t deserializator::read_uint16_le()
+{
+	if (this->size() < sizeof(uint16_t)) {
+		throw std::invalid_argument("deserializator::read_uint16_le(): buffer has less bytes then needed");
+	}
+
+	auto ret = utki::deserialize16le(this->data.data());
+	this->data = this->data.subspan(sizeof(uint16_t));
+	return ret;
+}
+
+uint32_t deserializator::read_uint32_le()
+{
+	if (this->size() < sizeof(uint32_t)) {
+		throw std::invalid_argument("deserializator::read_uint32_le(): buffer has less bytes then needed");
+	}
+
+	auto ret = utki::deserialize32le(this->data.data());
+	this->data = this->data.subspan(sizeof(uint32_t));
+	return ret;
+}
+
+uint64_t deserializator::read_uint64_le()
+{
+	if (this->size() < sizeof(uint64_t)) {
+		throw std::invalid_argument("deserializator::read_uint64_le(): buffer has less bytes then needed");
+	}
+
+	auto ret = utki::deserialize64le(this->data.data());
+	this->data = this->data.subspan(sizeof(uint64_t));
+	return ret;
+}
+
+float deserializator::read_float_le()
+{
+	if (this->size() < sizeof(float)) {
+		throw std::invalid_argument("deserializator::read_float_le(): buffer has less bytes then needed");
+	}
+
+	auto ret = utki::deserialize_float_le(this->data.data());
+	this->data = this->data.subspan(sizeof(float));
+	return ret;
+}
+
+uint16_t deserializator::read_uint16_be()
+{
+	if (this->size() < sizeof(uint16_t)) {
+		throw std::invalid_argument("deserializator::read_uint16_be(): buffer has less bytes then needed");
+	}
+
+	auto ret = utki::deserialize16be(this->data.data());
+	this->data = this->data.subspan(sizeof(uint16_t));
+	return ret;
+}
+
+uint32_t deserializator::read_uint32_be()
+{
+	if (this->size() < sizeof(uint32_t)) {
+		throw std::invalid_argument("deserializator::read_uint32_be(): buffer has less bytes then needed");
+	}
+
+	auto ret = utki::deserialize32be(this->data.data());
+	this->data = this->data.subspan(sizeof(uint32_t));
+	return ret;
+}
+
+uint64_t deserializator::read_uint64_be()
+{
+	if (this->size() < sizeof(uint64_t)) {
+		throw std::invalid_argument("deserializator::read_uint64_be(): buffer has less bytes then needed");
+	}
+
+	auto ret = utki::deserialize64be(this->data.data());
+	this->data = this->data.subspan(sizeof(uint64_t));
+	return ret;
+}
---
+++ tests/unit/src/deserializator.cpp
@@ -0,0 +1,159 @@
+#include <tst/check.hpp>
+#include <tst/set.hpp>
+#include <utki/deserializator.hpp>
+#include <utki/string.hpp>
+#include <utki/util.hpp>
+
+using namespace std::string_view_literals;
+
+namespace {
+tst::set set("deserializator", [](tst::suite& suite) {
+	suite.add("read_string", []() {
+		auto str = "bbbHello world!ccc"sv;
+
+		utki::deserializator d(utki::to_uint8_t(str));
+
+		auto bbb = d.read_string(3);
+		auto hw = d.read_string(12);
+		auto ccc = d.read_string(3);
+
+		tst::check_eq(bbb, "bbb"sv, SL);
+		tst::check_eq(hw, "Hello world!"sv, SL);
+		tst::check_eq(ccc, "ccc"sv, SL);
+	});
+
+	suite.add("read_span", []() {
+		auto str = "bbbHello world!ccc"sv;
+
+		utki::deserializator d(utki::to_uint8_t(str));
+
+		auto bbb = d.read_span(3);
+		auto hw = d.read_span(12);
+		auto ccc = d.read_span(3);
+
+		tst::check_eq(utki::make_string_view(bbb), "bbb"sv, SL);
+		tst::check_eq(utki::make_string_view(hw), "Hello world!"sv, SL);
+		tst::check_eq(utki::make_string_view(ccc), "ccc"sv, SL);
+	});
+
+	suite.add("read_uint16_le", []() {
+		std::vector<uint8_t> buf = {0xa1, 0xb2};
+
+		utki::deserializator d(buf);
+
+		tst::check(!d.empty(), SL);
+		tst::check_eq(d.size(), sizeof(uint16_t), SL);
+
+		auto u16 = d.read_uint16_le();
+
+		tst::check_eq(u16, uint16_t(0xb2a1), SL);
+		tst::check(d.empty(), SL);
+		tst::check_eq(d.size(), size_t(0), SL);
+	});
+
+	suite.add("read_uint32_le", []() {
+		std::vector<uint8_t> buf = {0xa1, 0xb2, 0xc3, 0xd4};
+
+		utki::deserializator d(buf);
+
+		tst::check(!d.empty(), SL);
+		tst::check_eq(d.size(), sizeof(uint32_t), SL);
+
+		auto u32 = d.read_uint32_le();
+
+		tst::check_eq(u32, uint32_t(0xd4c3b2a1), SL);
+		tst::check(d.empty(), SL);
+		tst::check_eq(d.size(), size_t(0), SL);
+	});
+
+	suite.add("read_uint64_le", []() {
+		std::vector<uint8_t> buf = {0xa1, 0xb2, 0xc3, 0xd4, 0xe5, 0xf6, 0x17, 0x28};
+
+		utki::deserializator d(buf);
+
+		tst::check(!d.empty(), SL);
+		tst::check_eq(d.size(), sizeof(uint64_t), SL);
+
+		auto u64 = d.read_uint64_le();
+
+		tst::check_eq(u64, uint64_t(0x2817f6e5d4c3b2a1), SL);
+		tst::check(d.empty(), SL);
+		tst::check_eq(d.size(), size_t(0), SL);
+	});
+
+	suite.add<std::pair<float, std::array<uint8_t, 4>>>( //
+		"read_float_le",
+		// clang-format off
+		{
+			{ 13, {0x00, 0x00, 0x50, 0x41}},
+			{ 13666, {0x00, 0x88, 0x55, 0x46}},
+			{ 13666e10f, {0x46, 0x95, 0xf8, 0x56}},
+			{ 13.666f, {0xf0, 0xa7, 0x5a, 0x41}},
+			{ -13, {0x00, 0x00, 0x50, 0xc1}},
+			{ -13666, {0x00, 0x88, 0x55, 0xc6}},
+			{ -13666e10f, {0x46, 0x95, 0xf8, 0xd6}},
+			{ -13.666f, {0xf0, 0xa7, 0x5a, 0xc1}},
+		},
+		// clang-format on
+		[](const auto& p) {
+			std::array<uint8_t, 4> buf{};
+			utki::serialize_float_le(p.first, buf.data());
+
+			tst::check(buf == p.second, SL);
+
+			utki::deserializator d(buf);
+
+			float f = d.read_float_le();
+
+			tst::check_eq(f, p.first, SL);
+			tst::check(d.empty(), SL);
+			tst::check_eq(d.size(), size_t(0), SL);
+		}
+	);
+
+	suite.add("read_uint16_be", []() {
+		std::vector<uint8_t> buf = {0xb2, 0xa1};
+
+		utki::deserializator d(buf);
+
+		tst::check(!d.empty(), SL);
+		tst::check_eq(d.size(), sizeof(uint16_t), SL);
+
+		auto u16 = d.read_uint16_be();
+
+		tst::check_eq(u16, uint16_t(0xb2a1), SL);
+		tst::check(d.empty(), SL);
+		tst::check_eq(d.size(), size_t(0), SL);
+	});
+
+	suite.add("read_uint32_be", []() {
+		std::vector<uint8_t> buf = {0xd4, 0xc3, 0xb2, 0xa1};
+
+		utki::deserializator d(buf);
+
+		tst::check(!d.empty(), SL);
+		tst::check_eq(d.size(), sizeof(uint32_t), SL);
+
+		auto u32 = d.read_uint32_be();
+
+		tst::check_eq(u32, uint32_t(0xd4c3b2a1), SL);
+		tst::check(d.empty(), SL);
+		tst::check_eq(d.size(), size_t(0), SL);
+	});
+
+	suite.add("read_uint64_be", []() {
+		std::vector<uint8_t> buf = {0x28, 0x17, 0xf6, 0xe5, 0xd4, 0xc3, 0xb2, 0xa1};
+
+		utki::deserializator d(buf);
+
+		tst::check(!d.empty(), SL);
+		tst::check_eq(d.size(), sizeof(uint64_t), SL);
+
+		auto u64 = d.read_uint64_be();
+
+		tst::check_eq(u64, uint64_t(0x2817f6e5d4c3b2a1), SL);
+		tst::check(d.empty(), SL);
+		tst::check_eq(d.size(), size_t(0), SL);
+	});
+});
+} // namespace
