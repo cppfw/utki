70d9c38f46cbd3e5f35e2216dc2b052cd45f64e4
Ivan Gagis <igagis@gmail.com>
2024-12-22 16:11:36 +0200

    add wasm config

---
+++ config/wasm.mk
@@ -0,0 +1,12 @@
+include $(config_dir)base/base.mk
+
+this_cxxflags += -O3
+
+this_cxx := em++
+this_cc := emcc
+this_ar := emar
+
+this_static_lib_only = : true
+
+this_cxxflags += -fwasm-exceptions
+this_ldflags += -fwasm-exceptions
